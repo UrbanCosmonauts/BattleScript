e508df1e7b7de9e7a4e501b833503a8179162c29
Timothy Quach <timothyquachbot@gmail.com>
2015-08-19 11:55:46 -0700

    Add collab socket skeleton

--- server/server.js
+++ server/server.js
@@ -26,18 +26,20 @@ io.on('connection', function(socket) {
   var handler = socket.handshake.query.handler;
   if (handler === 'dashboard') dashboardHandler(socket, io);
   if (handler === 'battle') battleHandler(socket, io);
+  if (handler === 'collab') collabHandler(socket, io);
 });
 
-// set up two handlers for separate sockets
+// set up handlers for separate sockets
 var battleHandler = require('./config/battleHandler.js');
 var dashboardHandler = require('./config/dashboardHandler.js');
+var collabHandler = require('./config/collabHandler.js');
 
 
 // For handling various sockets, goto socket battleHandler in config js
 // io.on('connection', function(socket){
 //   var handler = socket.handshake.query.handler;
 //   if (handler === 'battle') battleHandler(socket, io);
-//   
+//
 // });
 
 // export our app for testing and flexibility, required by index.js
