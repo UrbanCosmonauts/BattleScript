a3680ed0f084b6ec122d54c786ad4e05d1881efc
Timothy Quach <timothyquachbot@gmail.com>
2015-08-19 11:27:29 -0700

    Set up collab editor

--- client/app/collab/collab.js
+++ client/app/collab/collab.js
@@ -1,4 +1,11 @@
 angular.module('battlescript.collab', [])
-.controller('CollabController', function() {
+.controller('CollabController', function(Editor) {
   var vm = this; // vm refers to view-model
+
+  vm.setUpCollab = function() {
+    /**
+     * Sets up the Collaborative Editor
+     */
+    vm.editor = Editor.makeEditor('#editor--user', false);
+  }();
 })
